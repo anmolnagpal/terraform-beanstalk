46086a4c4dbc219985900348e1a3c93031860a67
Anmol Nagpal <ianmolnagpal@gmail.com>
2017-09-17 14:03:27 +0400

    update

---
+++ 10-vpc.tf
@@ -0,0 +1,21 @@
+#__     __  ____     ____
+#\ \   / / |  _ \   / ___|
+##\ \ / /  | |_) | | |
+###\ V /   |  __/  | |___
+####\_/    |_|      \____|
+
+# Create a VPC to launch our instances into
+resource "aws_vpc" "main" {
+  cidr_block           = "${var.vpc["cidr_block"]}"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags {
+    Name        = "${var.vpcName}"
+    Environment = "${var.env}"
+  }
+}
+
+output VPC {
+  value = "${aws_vpc.main.id}"
+}
