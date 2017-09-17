37681a2512189b93db491018a24201fe613bbd37
Anmol Nagpal <ianmolnagpal@gmail.com>
2017-09-17 13:43:32 +0400

    update

---
+++ basic.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  access_key = "XXX"
+  secret_key = "XXX"
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "pythonterradtcc" {
+  bucket = "pythonterradtcc.applicationversion.bucket"
+}
+
+resource "aws_s3_bucket_object" "pythonterradtcc" {
+  bucket = "${aws_s3_bucket.pythonterradtcc.id}"
+  key    = "beanstalkpython/pyterra.zip"
+  source = "pyterra.zip"
+}
+resource "aws_elastic_beanstalk_application" "pythonterradtcc" {
+  name = "pythonterradtcc"
+  description = "pythonterra"
+}
+
+resource "aws_elastic_beanstalk_application_version" "pythonterradtcc" {
+  name        = "pyterradtcc12"  
+  application = "${aws_elastic_beanstalk_application.pythonterradtcc.name}"
+  description = "Version latest of app ${aws_elastic_beanstalk_application.pythonterradtcc.name}"
+  bucket      = "${aws_s3_bucket.pythonterradtcc.id}"
+  key         = "${aws_s3_bucket_object.pythonterradtcc.id}"
+
+}
+
+resource "aws_elastic_beanstalk_environment" "pythonterradtcc" {
+  name                  = "pythonterradtcc"
+  application           = "${aws_elastic_beanstalk_application.pythonterradtcc.name}"
+  solution_stack_name   = "64bit Amazon Linux 2017.03 v2.5.1 running Python 3.4"
+  tier                  = "WebServer" 
+  version_label         = "pyterradtcc12"
+  
+}
+
+
+
